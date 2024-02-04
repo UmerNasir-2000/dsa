6629f817524860d003b6e08d880210cf3ec6c814
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-02-04 13:41:10 +0500

    feat: add method to find maximum consecutive ones in an array

--- app/src/main/java/array/Array.java
+++ app/src/main/java/array/Array.java
@@ -10,6 +10,27 @@ public class Array<T> {
         this.items = (T[]) new Object[this.capacity];
     }
 
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1)
+                count++;
+            
+            else {
+                if (max < count) 
+                    max = count;
+                count = 0;
+            }
+        }
+        
+        if(max < count)
+            max = count;
+            
+        return max;
+    }
+
     public void removeAt(int index) {
         this.validateIndex(index);
 
